bfe25a05c01d5051551e2ebce821101f4f790e2c
Stacey Astoveza <staceyastoveza@transfix.io>
2017-07-11 18:13:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+    return "#{array[0..1].join(", ")}, and #{array.last}"
+  else
+    total_values = array.length - 2
+    return "#{array[0..total_values].join(", ")}, and #{array.last}"
+  end
 end
